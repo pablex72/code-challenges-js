2aabfe76f31c64e1f1ab189161c6a7679c349b40
pablex72 <tonyp2hoy@gmail.com>
2022-12-21 02:09:45 -0400

    challenge filter reduce map ii js

--- exercise-map.js
+++ exercise-map.js
@@ -3,7 +3,7 @@
 function usingMap (){
 const input = [1, 2, 3, 4, 5];
 //written with Arrow function
-next = input.map((num) => Math.pow(num, 2));
+let next = input.map((num) => Math.pow(num, 2));
 console.log(input)
 console.log(next)
 }
@@ -14,7 +14,7 @@ usingMap();
 function sumPositive(){
     const input = [1, -4, 12, 0, -3, 29, -150];
 
-    next = input
+    let next = input
     .filter(function (num) {
         return num > 0;
     })
@@ -26,3 +26,24 @@ function sumPositive(){
     console.log(next);
 }
 sumPositive();
+
+//Get name initials
+//const input = "George Raymond Richard Martin"; --> "GRRM";
+function nameInitial(){
+    const input = "George Raymond Richard Martin";
+    let next = input.split(" ").map((word) => word[0]).join('');
+    console.log(next)
+}
+nameInitial();
+
+//n! with Map and Reduce
+function factorial(){
+    const input = 6;
+    let fact = new Array(input)
+    .fill(null)
+    .map((index) => index + 1)
+    //.reduce((accumulator, currentValue) => accumulator * currentValue);
+    console.log(null + 1);
+}
+
+factorial();
