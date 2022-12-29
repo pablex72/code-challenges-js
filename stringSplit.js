a40ca3176adbf5f8cb5afc724065e9cacf103b51
pablex72 <tonyp2hoy@gmail.com>
2022-12-28 23:46:09 -0400

    add challenge codewars stringsplit

---
+++ stringSplit.js
@@ -0,0 +1,18 @@
+// 'abc' =>  ['ab', 'c_']
+// 'abcdef' => ['ab', 'cd', 'ef'] 
+
+function solution(str){
+
+    var arrInitial = str.split('')
+    var result = []
+
+    for(let i = 0; i<arrInitial.length; i+=2){
+        if(arrInitial[i+1]) result.push(arrInitial[i]+arrInitial[i+1])
+        else result.push(arrInitial[i]+'_')
+    }
+
+    console.log(result);
+    return result; 
+}
+
+solution("hello");
